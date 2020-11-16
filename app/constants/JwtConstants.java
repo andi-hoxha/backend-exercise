8071286e293efa116fa91529866398b932fec633
LAPTOP-S59619M1\andih <andihoxha@odysseyprime.nl>
2020-11-16 22:24:22 +0100

    Minor changes

--- app/constants/JwtConstants.java
+++ app/constants/JwtConstants.java
@@ -2,7 +2,7 @@ package constants;
 
 public class JwtConstants {
     public static final String SECRET_ACCESS = "PrimeSecretKeyToGenerateJwtAccessToken";
-    public static final long EXPIRATION_TIME_ACCESS = 200_000_000;
+    public static final long EXPIRATION_TIME_ACCESS = 3600000;
     public static final String HEADER_KEY = "Authorization";
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final Integer TOKEN_PREFIX_INDEX = 7;
